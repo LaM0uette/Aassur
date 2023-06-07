c33932aa6ac2305686c8f143f18c523b35ffdb7b
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-07 14:33:34 +0200

    add Repositories empty and IRepository

---
+++ Aassur/Core/Services/Repository/RepositoryMySQL.cs
@@ -0,0 +1,38 @@
+﻿using Aassur.Core.Data;
+
+namespace Aassur.Core.Services.Repository;
+
+public class RepositoryMySQL<T> : IRepository<T> where T : class
+{
+    private readonly MySqlDbContext _dbContext;
+    
+    public RepositoryMySQL(MySqlDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<List<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Aassur/Core/Services/Repository/RepositorySQLite.cs
@@ -0,0 +1,38 @@
+﻿using Aassur.Core.Data;
+
+namespace Aassur.Core.Services.Repository;
+
+public class RepositorySQLite<T> : IRepository<T> where T : class
+{
+    private readonly SqliteDbContext _dbContext;
+    
+    public RepositorySQLite(SqliteDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<List<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
