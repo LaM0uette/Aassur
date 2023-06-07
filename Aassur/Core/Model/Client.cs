ff94531570e5ed9813dd7c7402e1ae2872dfe046
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-07 09:50:18 +0200

    reboot project

---
+++ Aassur/Core/Model/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aassur.Core.Model;
+
+public class Client
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public int? CivilityId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
