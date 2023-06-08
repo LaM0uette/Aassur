90ad2983b6ae349edcf59055b201822570b77d88
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-08 18:58:14 +0200

    revert

--- Aassur/Core/Model/Client.cs
+++ Aassur/Core/Model/Client.cs
@@ -1,12 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Aassur.Core.Model;
+﻿namespace Aassur.Core.Model;
 
 public class Client
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public int? CivilityId { get; set; }
