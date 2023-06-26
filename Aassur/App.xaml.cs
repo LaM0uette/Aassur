20b797907d27eeead08bd7c4df0a2026db656028
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-26 10:29:25 +0200

    Create Sqlite Db If Not Exist

--- Aassur/App.xaml.cs
+++ Aassur/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Aassur;
+﻿using System.Reflection;
+
+namespace Aassur;
 
 public partial class App : Application
 {
@@ -6,6 +8,21 @@ public partial class App : Application
     {
         InitializeComponent();
 
+        CreateSqliteDbIfNotExist();
+
         MainPage = new AppShell();
+    }
+
+    private static void CreateSqliteDbIfNotExist()
+    {
+        const string resourceName = "Aassur.Core.DataBase.Aassur.db";
+        var assembly = Assembly.GetExecutingAssembly();
+        var destinationFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Aassur", "Aassur.db");
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream is null) throw new InvalidOperationException("Could not find embedded resource");
+
+        using var fileStream = File.Create(destinationFile);
+        stream.CopyTo(fileStream);
     }
 }
