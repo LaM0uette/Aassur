ae4ac0f0fd9adb8865fc0bf2af79cfda1964a8a5
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-26 10:34:42 +0200

    add CreateSqliteDbIfNotExist for mobile support

--- Aassur/App.xaml.cs
+++ Aassur/App.xaml.cs
@@ -17,7 +17,10 @@ public partial class App : Application
     {
         const string resourceName = "Aassur.Core.DataBase.Aassur.db";
         var assembly = Assembly.GetExecutingAssembly();
-        var destinationFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Aassur", "Aassur.db");
+        
+        var destinationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Aassur");
+        Directory.CreateDirectory(destinationDirectory);
+        var destinationFile = Path.Combine(destinationDirectory, "Aassur.db");
 
         using var stream = assembly.GetManifestResourceStream(resourceName);
         if (stream is null) throw new InvalidOperationException("Could not find embedded resource");
