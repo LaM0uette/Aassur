6bf657a1905dffd8ea2b47d56c7453733ee3de3b
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-29 11:50:26 +0200

    test ContentButton

--- Aassur/Resources/Components/ContentButton.cs
+++ Aassur/Resources/Components/ContentButton.cs
@@ -1,15 +1,21 @@
 ﻿namespace Aassur.Resources.Components;
 
-public class StyledButton : Button
+public class ContentButton : ContentView
 {
-    public StyledButton()
+    public event EventHandler Clicked;
+
+    public ContentButton()
     {
+        var tapGestureRecognizer = new TapGestureRecognizer();
+        tapGestureRecognizer.Tapped += (s, e) => Clicked?.Invoke(this, EventArgs.Empty);
+        GestureRecognizers.Add(tapGestureRecognizer);
+        
         var primaryColor = Color.FromRgb(255, 0, 0);
         if (Application.Current is not null && Application.Current.Resources.TryGetValue("Primary", out var obj) && obj is Color color)
         {
             primaryColor = color;
         }
-        
+
         VisualStateManager.SetVisualStateGroups(this, new VisualStateGroupList
         {
             new()
