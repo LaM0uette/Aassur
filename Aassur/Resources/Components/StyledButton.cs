c1a4b0bb6f128fd2e0ef2ed52fcc3559a7f02b11
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-29 10:43:37 +0200

    add StyledButton (name temporaire)

---
+++ Aassur/Resources/Components/StyledButton.cs
@@ -0,0 +1,24 @@
+﻿namespace Aassur.Resources.Components;
+
+public class StyledButton : Button
+{
+    public StyledButton()
+    {
+        VisualStateManager.SetVisualStateGroups(this, new VisualStateGroupList
+        {
+            new()
+            {
+                Name = "CommonStates",
+                States =
+                {
+                    new VisualState { Name = "Normal" },
+                    new VisualState 
+                    { 
+                        Name = "PointerOver",
+                        Setters = { new Setter { Property = BackgroundColorProperty, Value = Color.FromRgb(255, 0, 0) } }
+                    }
+                }
+            }
+        });
+    }
+}
