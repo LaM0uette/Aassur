b9e6b0f27b9cb592c1e3d4ab1910c1b50ea4e430
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-01 14:58:49 +0200

    add MenuFicheClientButton + card client finish basic desing

---
+++ Aassur/Resources/Components/MenuFicheClientButton.cs
@@ -0,0 +1,48 @@
+﻿namespace Aassur.Resources.Components;
+
+public class MenuFicheClientButton : Button
+{
+    public MenuFicheClientButton()
+    {
+        var primaryColor = Color.FromRgb(255, 0, 0);
+        var tertiaryColor = Color.FromRgb(0, 255, 0);
+        
+        if (Application.Current is not null && Application.Current.Resources.TryGetValue("Primary", out var obj) && obj is Color color)
+        {
+            primaryColor = color;
+        }
+        
+        if (Application.Current is not null && Application.Current.Resources.TryGetValue("Tertiary", out var obj2) && obj2 is Color color2)
+        {
+            tertiaryColor = color2;
+        }
+        
+        VisualStateManager.SetVisualStateGroups(this, new VisualStateGroupList
+        {
+            new()
+            {
+                Name = "CommonStates",
+                States =
+                {
+                    new VisualState 
+                    { 
+                        Name = "Normal",
+                        Setters =
+                        {
+                            new Setter { Property = BackgroundColorProperty, Value = tertiaryColor }
+                        }
+                    },
+                    new VisualState 
+                    { 
+                        Name = "PointerOver",
+                        Setters = 
+                        { 
+                            new Setter { Property = BackgroundColorProperty, Value = primaryColor },
+                            new Setter { Property = TextColorProperty, Value = tertiaryColor } 
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
