39f0b886b6e9361a8da4aa0331265ea8a6c9932b
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-02 16:30:26 +0200

    add FilteredEntry with EntrySearch

---
+++ Aassur/Resources/Components/FilteredEntry.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Aassur.Resources.Components;
+
+public partial class FilteredEntry
+{
+    public FilteredEntry()
+    {
+        InitializeComponent();
+    }
+    
+    private void OnEntrySearchTextChanged(object sender, TextChangedEventArgs e)
+    {
+    }
+    
+    void OnPickerSearchSelectedIndexChanged(object sender, EventArgs e)
+    {
+    }
+}
