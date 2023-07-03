5900d115cb5f0162d98003cea4e855f6dc7ce066
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-03 17:15:43 +0200

    upgrade ui MainPage

--- Aassur/Core/Data/DbData.cs
+++ Aassur/Core/Data/DbData.cs
@@ -5,13 +5,19 @@ namespace Aassur.Core.Data;
 
 public class DbData
 {
+    #region Statements
+
     public IEnumerable<Client> Clients { get; set; } = new List<Client>();
 
     public DbData()
     {
         Task.Run(SetClients);
     }
-    
+
+    #endregion
+
+    #region Functions
+
     private async void SetClients()
     {
         var clients = await GetAllClientsAsync();
@@ -22,4 +28,6 @@ public class DbData
     {
         return await SqliteService.Client.GetAllAsync();
     }
+
+    #endregion
 }
