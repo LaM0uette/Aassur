5d4deee1886674de88a6d754f72b08d5d5722640
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-03 18:18:14 +0200

    refacoting ClientElementsFactory

--- Aassur/Core/Factory/ClientElementsFactory.cs
+++ Aassur/Core/Factory/ClientElementsFactory.cs
@@ -3,45 +3,60 @@ using Microsoft.Maui.Controls.Shapes;
 
 namespace Aassur.Core.Factory;
 
-public class ClientElementsFactory
+public static class ClientElementsFactory
 {
     public static Grid CreateClientElements(string clientInfo)
     {
+        var grid = CreateGrid(2);
+            
+        var label = CreateLabel(clientInfo);
+        AddElementToGrid(grid, label, row: 0, column: 0);
+
+        var menuButton = new MenuFicheClientButton();
+        AddElementToGrid(grid, menuButton, row: 0, column: 1);
+
+        var line = CreateLine();
+        AddElementToGrid(grid, line, row: 1, column: 0, columnSpan: 2);
+            
+        return grid;
+    }
+
+    private static Grid CreateGrid(int size)
+    {
         var grid = new Grid();
-
-        grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-
-        var label = new Label
+        for (int i = 0; i < size; i++)
         {
-            Text = clientInfo,
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+        }
+
+        return grid;
+    }
+
+    private static Label CreateLabel(string text)
+    {
+        return new Label
+        {
+            Text = text,
             TextColor = Color.FromRgb(255, 0, 0),
             VerticalTextAlignment = TextAlignment.End
         };
-        Grid.SetRow(label, 0);
-        Grid.SetColumn(label, 0);
+    }
 
-        grid.Children.Add(label);
-
-        var menuButton = new MenuFicheClientButton();
-
-        Grid.SetRow(menuButton, 0);
-        Grid.SetColumn(menuButton, 1);
-
-        grid.Children.Add(menuButton);
-
-        var line = new Line
+    private static Line CreateLine()
+    {
+        return new Line
         {
             StrokeThickness = 2,
-            BackgroundColor = Color.FromRgba(0, 255, 0, 0.1),
+            BackgroundColor = Color.FromRgba(0, 255, 0, 0.1)
         };
-        Grid.SetRow(line, 1);
-        Grid.SetColumn(line, 0);
-        Grid.SetColumnSpan(line, 2);
+    }
 
-        grid.Children.Add(line);
-        
-        return grid;
+    private static void AddElementToGrid(Grid grid, View element, int row, int column, int columnSpan = 1)
+    {
+        Grid.SetRow(element, row);
+        Grid.SetColumn(element, column);
+        Grid.SetColumnSpan(element, columnSpan);
+        grid.Children.Add(element);
     }
 }
