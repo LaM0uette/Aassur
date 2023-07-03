13c9559b3dc371410c2adf1b322d0ca50aa0ebb1
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-03 18:13:20 +0200

    create ClientElementsFactory for recent client (not finish)

--- Aassur/MainPage.xaml.cs
+++ Aassur/MainPage.xaml.cs
@@ -1,9 +1,37 @@
-﻿namespace Aassur;
+﻿using Aassur.Core.Factory;
+
+namespace Aassur;
 
 public partial class MainPage
 {
+    #region Statements
+
     public MainPage()
     {
         InitializeComponent();
+        
+        Task.Run(Test);
     }
+
+    #endregion
+    
+    #region Functions
+
+    private async void Test()
+    {
+        while (!App.DbData.Clients.Any()){await Task.Delay(100); }
+        
+        var clientsList = App.DbData.Clients
+            .Where(client => client.LastModificationDate.HasValue)
+            .OrderByDescending(client => client.LastModificationDate)
+            .Take(3)
+            .ToList();
+
+        foreach(var client in clientsList)
+        {
+            StackLayoutRecentClients.Children.Add(ClientElementsFactory.CreateClientElements(client.FullName));
+        }
+    }
+
+    #endregion
 }
