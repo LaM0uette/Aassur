456eb4093396da9ec9f37bd874ef5933e6038071
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-04 11:48:41 +0200

    refactor App and add DatabaseInitializer

--- Aassur/App.xaml.cs
+++ Aassur/App.xaml.cs
@@ -1,37 +1,25 @@
-﻿using System.Reflection;
-using Aassur.Core.Data;
+﻿using Aassur.Core.Data;
+using Aassur.Core.DataBase;
 
 namespace Aassur;
 
 public partial class App
 {
+    #region Statements
+
     public static DbData DbData { get; } = new();
     
     public App()
     {
-        CreateSqliteDbIfNotExist();
+        DatabaseInitializer.CreateSqliteDbIfNotExist();
         InitializeComponent();
-        
         MainPage = new AppShell();
     }
 
-    private static void CreateSqliteDbIfNotExist()
-    {
-        const string resourceName = "Aassur.Core.DataBase.Aassur.db";
-        var assembly = Assembly.GetExecutingAssembly();
-        
-        var destinationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Aassur");
-        Directory.CreateDirectory(destinationDirectory);
-        var destinationFile = Path.Combine(destinationDirectory, "Aassur.db");
+    #endregion
 
-        using var stream = assembly.GetManifestResourceStream(resourceName);
-        if (stream is null) throw new InvalidOperationException("Could not find embedded resource");
+    #region Functions
 
-        if (File.Exists(destinationFile)) return;
-        using var fileStream = File.Create(destinationFile);
-        stream.CopyTo(fileStream);
-    }
-    
     protected override Window CreateWindow(IActivationState activationState)
     {
         var window = base.CreateWindow(activationState);
@@ -46,4 +34,6 @@ public partial class App
 
         return window;
     }
+
+    #endregion
 }
---
+++ Aassur/Core/DataBase/DatabaseInitializer.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+
+namespace Aassur.Core.DataBase;
+
+public class DatabaseInitializer
+{
+    private const string _resourceName = "Aassur.Core.DataBase.Aassur.db";
+    private const string _destinationDirectoryName = "Aassur";
+    private const string _destinationFileName = "Aassur.db";
+
+    public static void CreateSqliteDbIfNotExist()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var destinationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), _destinationDirectoryName);
+
+        CreateDirectory(destinationDirectory);
+
+        var destinationFile = Path.Combine(destinationDirectory, _destinationFileName);
+
+        if (FileExists(destinationFile)) 
+            return;
+
+        using var stream = GetEmbeddedResourceStream(assembly, _resourceName);
+        CreateFileFromStream(destinationFile, stream);
+    }
+
+    private static void CreateDirectory(string directory)
+    {
+        Directory.CreateDirectory(directory);
+    }
+
+    private static bool FileExists(string file)
+    {
+        return File.Exists(file);
+    }
+
+    private static Stream GetEmbeddedResourceStream(Assembly assembly, string resourceName)
+    {
+        var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream == null) throw new InvalidOperationException("Could not find embedded resource");
+
+        return stream;
+    }
+
+    private static void CreateFileFromStream(string destinationFile, Stream stream)
+    {
+        using var fileStream = File.Create(destinationFile);
+        stream.CopyTo(fileStream);
+    }
+}
