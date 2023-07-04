5428f95b61d4a5baa59613853ff672590d1f7632
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-04 14:36:47 +0200

    up project

--- Aassur/AppShell.xaml.cs
+++ Aassur/AppShell.xaml.cs
@@ -11,6 +11,6 @@ public partial class AppShell
         //     : new ShellContent {Content = new MainPage()});
         
         // TODO: Remove this after finishing the mobile version
-        Items.Add(new ShellContent {Content = new MainPageMobile()});
+        Items.Add(new ShellContent {Content = new MainPage()});
     }
 }
