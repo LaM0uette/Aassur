7d5247b999e0e4d31d81666da47a906b759a1852
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-05 10:08:13 +0200

    add ButtonTabControl and style

--- Aassur/Pages/ClientPage.xaml.cs
+++ Aassur/Pages/ClientPage.xaml.cs
@@ -15,4 +15,17 @@ public partial class ClientPage : ContentPage
     }
 
     #endregion
+
+    private void ButtonTabControl_OnClicked(object sender, EventArgs e)
+    {
+        var primaryColor = Color.FromRgb(55, 190, 189);
+        var tertiaryColor = Color.FromRgb(58, 71, 70);
+        
+        ButtonTabControl0.BackgroundColor = tertiaryColor;
+        ButtonTabControl1.BackgroundColor = tertiaryColor;
+        ButtonTabControl2.BackgroundColor = tertiaryColor;
+
+        if (sender is Button button)
+            button.BackgroundColor = primaryColor;
+    }
 }
