31a2bd82088e4ca02f18941880c417c1d12b7474
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-10 17:19:02 +0200

    Add more client details to Profile Form

--- Aassur/Core/Factory/ProfilFormFactory.cs
+++ Aassur/Core/Factory/ProfilFormFactory.cs
@@ -15,5 +15,33 @@ public class ProfilFormFactory : FormFactory
     
     protected override void CreateLabels(Layout grid, Client client)
     {
+        var clientData = GetClientData(client);
+        
+        var labelCountryOfResidence = CreateLabel(clientData.CountryOfResidence);
+        AddElement(grid, labelCountryOfResidence, 0, 1);
+        
+        var labelDateOfBirth = CreateLabel(clientData.DateOfBirth);
+        AddElement(grid, labelDateOfBirth, 1, 1);
+        
+        var labelFamilyStatus = CreateLabel(clientData.FamilyStatus);
+        AddElement(grid, labelFamilyStatus, 2, 1);
+        
+        var labelFunction = CreateLabel(client.Function);
+        AddElement(grid, labelFunction, 3, 1);
+        
+        var labelFoyer = CreateLabel(client.Foyer);
+        AddElement(grid, labelFoyer, 4, 1);
+        
+        var labelHobbies = CreateLabel(client.Hobbies);
+        AddElement(grid, labelHobbies, 5, 1);
+        
+        var labelRelatedCustomersClientId = CreateLabel(client.RelatedCustomersClientId.ToString());
+        AddElement(grid, labelRelatedCustomersClientId, 6, 1);
+        
+        var labelCreationDate = CreateLabel(client.CreationDate);
+        AddElement(grid, labelCreationDate, 7, 1);
+        
+        var labelOrigin = CreateLabel(client.Origin);
+        AddElement(grid, labelOrigin, 8, 1);
     }
 }
